c64f962df81707e44925453442c3e163f6745a34
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2023-12-15 01:01:32 +0300

    second

--- src/pages/Header/data/socialMedia.jsx
+++ src/pages/Header/data/socialMedia.jsx
@@ -1,17 +1,20 @@
 import { BsLinkedin } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaTelegram } from "react-icons/fa6";
 
 const SOCIAL_MEDIA = [
   {
     href: "facebook.com",
-    icon: <BsLinkedin />,
+    icon: <FaFacebook />,
   },
   {
     href: "facebook.com",
-    icon: <BsLinkedin />,
+    icon: <FaSquareXTwitter />,
   },
   {
     href: "facebook.com",
-    icon: <BsLinkedin />,
+    icon: <FaTelegram />,
   },
   {
     href: "facebook.com",
